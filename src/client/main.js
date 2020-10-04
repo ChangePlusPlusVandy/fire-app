3da961b67eb53402bcceecd6f8d85313dfdb430a
James Raubenheimer <raubenheimerjames@gmail.com>
2020-10-04 17:59:43 -0500

    Project and route setup

---
+++ src/client/main.js
@@ -0,0 +1,13 @@
+/* Copyright G. Hemingway, @2020 - All rights reserved */
+"use strict";
+
+import React, { useState } from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
+
+const MyApp = () => {
+  return;
+  <BrowserRouter></BrowserRouter>;
+};
+
+render(<MyApp />, document.getElementById("mainDiv"));
