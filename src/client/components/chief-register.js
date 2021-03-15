6ca709e50906d477dabdcfebc7b98536f9cdc236
Intiser <irparash@gmail.com>
2021-03-14 20:58:25 -0500

    added phone and department field to firechief schema; deleted city field

--- src/client/components/chief-register.js
+++ src/client/components/chief-register.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import PropTypes from "prop-types";
 import {
     ErrorMessage,
@@ -14,70 +14,71 @@ import {
     Checkbox, FireRegisterContainer,
 } from "./shared";
 import styled from "styled-components";
-import { validPassword, validUsername } from "../../shared";
-import { TitleLine, FreeButton, OwnerRegisterContainer, NameRegisterContainer } from "./shared"
-import { Link } from "react-router-dom";
+import {validPassword, validUsername} from "../../shared";
+import {TitleLine, FreeButton, OwnerRegisterContainer, NameRegisterContainer} from "./shared"
+import {Link} from "react-router-dom";
 
 
-export const ChiefRegister = ({ history }) => {
-let [state, setState] = useState({
-    first_name: "",
-    last_name: "",
-    email: "",
-    phone: "",
-    username: "",
-    password: "",
-    department: "",
+export const ChiefRegister = ({history}) => {
+    let [state, setState] = useState({
+        first_name: "",
+        last_name: "",
+        email: "",
+        phone: "",
+        username: "",
+        password: "",
+        department: "",
     });
     let [error, setError] = useState("");
     let [notify, setNotify] = useState("");
 
     // When user makes changes, update the state
     const onChange = (ev) => {
-      setError("");
-      // Update from form and clear errors
-      setState({
-        ...state,
-        [ev.target.name]: ev.target.value,
-      });
-      // Make sure the username is valid
-      if (ev.target.name === "username") {
-        let usernameInvalid = validUsername(ev.target.value);
-        if (usernameInvalid) setError(`Error: ${usernameInvalid.error}`);
-      }
-      // Make sure password is valid
-      else if (ev.target.name === "password") {
-        let pwdInvalid = validPassword(ev.target.value);
-        if (pwdInvalid) setError(`Error: ${pwdInvalid.error}`);
-      }
+        setError("");
+        // Update from form and clear errors
+        setState({
+            ...state,
+            [ev.target.name]: ev.target.value,
+        });
+        // Make sure the username is valid
+        if (ev.target.name === "username") {
+            let usernameInvalid = validUsername(ev.target.value);
+            if (usernameInvalid) setError(`Error: ${usernameInvalid.error}`);
+        }
+        // Make sure password is valid
+        else if (ev.target.name === "password") {
+            let pwdInvalid = validPassword(ev.target.value);
+            if (pwdInvalid) setError(`Error: ${pwdInvalid.error}`);
+        }
     };
 
     // On form submit, push information to database
     const onSubmit = async (ev) => {
-      ev.preventDefault();
-      // Only proceed if there are no errors
-      if (error !== "") return;
-      const res = await fetch("/v1/firechief", {
-        method: "POST",
-        body: JSON.stringify(state),
-        credentials: "include",
-        headers: {
-          "content-type": "application/json",
-        },
-      });
-      if (res.ok) {
-        // Notify users
-        setNotify(`${state.username} registered.  You will now need to log in.`);
-      } else {
-        const err = await res.json();
-        setError(err.error);
-      }
+        console.log("hey whatever");
+        ev.preventDefault();
+        // Only proceed if there are no errors
+        if (error !== "") return;
+        const res = await fetch("/v1/firechief", {
+            method: "POST",
+            body: JSON.stringify(state),
+            credentials: "include",
+            headers: {
+                "content-type": "application/json",
+            },
+        });
+        if (res.ok) {
+            // Notify users
+            setNotify(`${state.username} registered.  You will now need to log in.`);
+        } else {
+            const err = await res.json();
+            setError(err.error);
+        }
     };
 
     //
-    // const onAcceptRegister = () => {
-    //   history.push("/login");
-    // };
+    const onAcceptRegister = () => {
+      history.push("/login");
+    };
 
     // new onChange
     // const onChange = (ev) => {
@@ -138,9 +139,13 @@ let [state, setState] = useState({
             </select>
             <div style={{marginTop: "10px"}}>
                 <input className="consent-checkbox" type="checkbox" id="consent-agree"/>
-                <label style={{marginLeft: "6px"}} htmlFor="consent-agree">I agree to the <span style={{color:"389BFF", textDecoration:"none", cursor:"pointer"}}>terms and conditions</span></label>
+                <label style={{marginLeft: "6px"}} htmlFor="consent-agree">I agree to the <span style={{
+                    color: "389BFF",
+                    textDecoration: "none",
+                    cursor: "pointer"
+                }}>terms and conditions</span></label>
             </div>
-            <Link to="hubs"><FreeButton onClick={onSubmit} style={{backgroundColor:"#CB0000", marginTop: "18px"}}>Sign Up</FreeButton></Link>
+            <FreeButton onClick={onSubmit} style={{backgroundColor: "#CB0000", marginTop: "18px"}}>Sign Up</FreeButton>
         </FireRegisterContainer>
     );
 };
