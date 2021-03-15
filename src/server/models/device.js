5da36e73c889020f2018e897124e240ec44dc7a3
Intiser <irparash@gmail.com>
2021-03-15 02:49:17 -0500

    removed "unique" requirement from fields in Device schema that were creating conflict

--- src/server/models/device.js
+++ src/server/models/device.js
@@ -6,15 +6,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Zone = require("./zone");
 
-/***************** User Model *******************/
+/***************** Device Model *******************/
 
 let Device = new Schema({
   owner: { type: String, required: true },
   api_key: { type: String, required: true },
-  owner_id: { type: String, index: { unique: true } },
+  owner_id: { type: String },
   owner_name: { type: String, default: "" },
-  owner_email: { type: String, index: { unique: true } },
-  id: { type: String, index: { unique: true } },
+  owner_email: { type: String },
+  id: { type: String },
   city: { type: String, default: "" },
   latitude: { type: Number, default: 0 },
   longitude: { type: Number, default: 0 },
